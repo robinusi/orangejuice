573d6b80683de36068f2d0c1bdb8486e10d584cd
Robin Usi <usirobin@gmail.com>
2017-11-27 19:56:24 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+.top-navigation {
+    height:74px;
+  width:100%;
+    display: inline-block;
+  background-color: white;
+  color:white;
+  font-size: 17px;
+  text-align: right;
+  }
+li{
+  display: inline;
+  margin-right: 20px;
+  font-family: 'Roboto', sans-serif;
+}
+
+
+.brand{
+    float: left;
+    margin-left: 1em;
+   
+  h1{
+    margin: 0;
+    font-size:1.5em;
+  }
+    a{
+      //.link-style;
+    }
+  }
